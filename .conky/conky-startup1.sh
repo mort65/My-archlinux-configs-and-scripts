dc63f7184bfea4e313725edc92b76b3bdccc9844
mort65 <mohsenbani65@gmail.com>
2018-01-14 22:42:31 +0000

    Update conky-startup1.sh

--- .conky/conky-startup1.sh
+++ .conky/conky-startup1.sh
@@ -1,5 +1,8 @@
+#!/bin/sh
+USERNAME=${SUDO_USER:-$(id -u -n)}
+HOMEDIR="/home/$USERNAME"
 if [[ ( $DESKTOP_SESSION == "/usr/share/xsessions/xfce" ) || ( $DESKTOP_SESSION == "/usr/share/xsessions/mate" ) || ( $DESKTOP_SESSION == "/usr/share/xsessions/budgie-desktop" ) || ( $DESKTOP_SESSION == "/usr/share/xsessions/cinnamon" ) || ( $DESKTOP_SESSION == "/usr/share/xsessions/lxqt" ) || ( $DESKTOP_SESSION == "/usr/share/xsessions/openbox" ) ]]
 then
-   . /home/mohsen/.conky/conky-startup.sh
+   . "$HOMEDIR/.conky/conky-startup.sh"
 fi
 exit
