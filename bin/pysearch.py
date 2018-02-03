32153aa12c5bce475fe836bc51d6077c158bf012
mort65 <mohsenbani65@gmail.com>
2018-02-03 21:46:09 +0000

    Update pysearch.py

--- bin/pysearch.py
+++ bin/pysearch.py
@@ -17,31 +17,8 @@ class Py_Search:
     mark = False
     usage = False
     sort = False
-
-    def show_usage(self):
-            print("""Usage: pysearch [OPTION]... [PATH] [TEXT]
-search for TEXT in the given PATH.
-Text can be a regular expression.
-
-  -h, --help                 display this help 
-  -r, --recursive            search recursively  
-  -f, --file                 search for files
-  -l, --link                 search for links
-  -d, --dir                  search for directories
-  -m, --mark                 mark files and dirs with f and d
-  -i, --ignore-case          ignore case in the search result    
-  -n, --name                 don't show fullpath in the result
-  -s, --sort                 sort the search result alphabetically 
-  
-Examples:
-  pysearch -if /usr/bin vim    "Find files in bin dir that have 
-  'vim' in their names and ignore case sensitivity."
-  pysearch -lr /usr/bin 'vim$' "Recursively find all links in 
-  bin dir that have 'vim' at the end of their names.
-  pysearch -rs /usr/bin '^im'  "Find recursively everything 
-  in bin that starts with 'im' and only write short names."
-  pysearch -dr /usr/bin '.*'   "Recursively find all dirs in bin" """)
-
+    silent = False
+    
     def add_result(self,parent_dir,new_result,is_dir):
         if self.recursive:
             if is_dir:
@@ -106,8 +83,9 @@ Examples:
             else:
                 result_link += "f"
         self.result.append(result_link)
-        if not self.sort:
-            print(result_link)
+        if not self.silent:
+            if not self.sort:
+                print(result_link)
 
     def add_dir(self,new_dir):
         result_dir = ""
@@ -116,8 +94,9 @@ Examples:
         else:
             result_dir = "\""+new_dir+"\""
         self.result.append(result_dir)
-        if not self.sort:
-            print(result_dir)
+        if not self.silent:
+            if not self.sort:
+                print(result_dir)
 
     def add_file(self,new_file):
         result_file = ""
@@ -126,81 +105,29 @@ Examples:
         else:
             result_file = "\""+new_file+"\""
         self.result.append(result_file)
-        if not self.sort:
-            print(result_file)
+        if not self.silent:
+            if not self.sort:
+                print(result_file)
             
-    def __init__(self,args):
-        if len(args) > 1:
-            for i in range(1,len(args)):
-                if args[i][0] == '-':
-                    if (self.main_dir != '') or (self.text != ''):
-                        self.show_usage()
-                        exit(1)
-                    elif args[i][1] == '-':
-                        if args[i] == '--help':
-                            self.usage = True
-                        elif args[i] == '--recursive':
-                            self.recursive = True
-                        elif args[i] == '--ignore-case':
-                            self.ignorecase = True
-                        elif args[i] == '--link':
-                            self.searchlink = True                   
-                        elif args[i] == '--dir':
-                            self.searchdir = True
-                        elif args[i] == '--name':
-                            self.onlyname = True
-                        elif args[i] == '--file':
-                            self.searchfile = True
-                        elif args[i] == '--mark':
-                            self.mark = True
-                        elif args[i] == '--sort':
-                            self.sort = True
-                        else:
-                            self.show_usage()
-                            exit(1)
-                    else:
-                        for c in range(1,len(args[i])):
-                            if args[i][c] == 'h':
-                                self.usage = True
-                            elif args[i][c] == 'r':
-                                self.recursive = True
-                            elif args[i][c] == 'i':
-                                self.ignorecase = True
-                            elif args[i][c] == 'l':
-                                self.searchlink = True                                              
-                            elif args[i][c] == 'd':
-                                self.searchdir = True
-                            elif args[i][c] == 'n':
-                                self.onlyname = True
-                            elif args[i][c] == 'f':
-                                self.searchfile = True
-                            elif args[i][c] == 'm':
-                                self.mark = True 
-                            elif args[i][c] == 's':
-                                self.sort = True
-                            else:
-                                self.show_usage()
-                                exit(1)
-                elif i == ( len(args) - 2 ):
-                    self.main_dir = args[i]
-                elif i == ( len(args) - 1 ):
-                    self.text = args[i]
-                    if self.ignorecase:
-                        self.pattern = self.text.lower()
-                    else:
-                        self.pattern = self.text
-                else:
-                    self.show_usage()
-                    exit(1)
-        if self.usage:
-            self.show_usage()
-            exit(0)
+    def __init__(self,Main_Dir='.',Pattern='.*',Recursive=False,Ignorecase=False,Searchdir=False,Searchfile=False,Searchlink=False,Onlyname=False,Mark=False,Sort=False,Silent=False):
+        self.main_dir = Main_Dir
+        self.pattern = Pattern
+        self.recursive = Recursive
+        self.ignorecase = Ignorecase
+        self.searchdir = Searchdir
+        self.searchfile = Searchfile
+        self.searchlink = Searchlink
+        self.onlyname = Onlyname
+        self.mark = Mark
+        self.sort = Sort
+        self.silent = Silent
         if self.searchdir is False and self.searchlink is False and self.searchfile is False:
-            self.searchdir,self.searchfile,self.searchlink = True,True,True
+            return
         if self.main_dir == '':
-            self.main_dir =os.path.realpath('.')
+            return
         if self.pattern == '':
-            self.pattern = '.*'
+            return
+
     def main(self):
         try:
             if self.recursive:
@@ -248,11 +175,120 @@ Examples:
                     self.result = sorted(self.result,key=str.lower)
                 else:
                     self.result = sorted(self.result)
-                for i in self.result:
-                    print(i)
+                if not self.silent:
+                    for i in self.result:
+                        print(i)
         except:
             pass
 
+def show_usage():
+    print("""Usage: pysearch [OPTION]... [PATH] [TEXT]
+search for TEXT in the given PATH.
+Text can be a regular expression.
+
+  -h, --help                 display this help 
+  -r, --recursive            search recursively  
+  -f, --file                 search for files
+  -l, --link                 search for links
+  -d, --dir                  search for directories
+  -m, --mark                 mark files and dirs with f and d
+  -i, --ignore-case          ignore case in the search result    
+  -n, --name                 don't show fullpath in the result
+  -s, --sort                 sort the search result alphabetically 
+  
+Examples:
+  pysearch -if /usr/bin vim    "Find files in bin dir that have 
+  'vim' in their names and ignore case sensitivity."
+  pysearch -lr /usr/bin 'vim$' "Recursively find all links in 
+  bin dir that have 'vim' at the end of their names.
+  pysearch -rs /usr/bin '^im'  "Find recursively everything 
+  in bin that starts with 'im' and only write short names."
+  pysearch -dr /usr/bin '.*'   "Recursively find all dirs in bin" """)
+
 if __name__ == "__main__":
-    pys = Py_Search(sys.argv)
+    Main_Dir = ''
+    Text = ''
+    Pattern = ''
+    Recursive = False
+    Ignorecase = False
+    Searchdir = False
+    Searchfile = False
+    Onlyname = False
+    Searchlink = False
+    Mark = False
+    Usage = False
+    Sort = False
+    if len(sys.argv) > 1:
+        for i in range(1,len(sys.argv)):
+            if len(sys.argv[i]) > 0:
+                if sys.argv[i][0] == '-':
+                    if (Main_Dir != '') or (Text != ''):
+                        show_usage()
+                        exit(1)
+                    elif sys.argv[i][1] == '-':
+                        if sys.argv[i] == '--help':
+                            Usage = True
+                        elif sys.argv[i] == '--Recursive':
+                            Recursive = True
+                        elif sys.argv[i] == '--ignore-case':
+                            Ignorecase = True
+                        elif sys.argv[i] == '--link':
+                            Searchlink = True                   
+                        elif sys.argv[i] == '--dir':
+                            Searchdir = True
+                        elif sys.argv[i] == '--name':
+                            Onlyname = True
+                        elif sys.argv[i] == '--file':
+                            Searchfile = True
+                        elif sys.argv[i] == '--Mark':
+                            Mark = True
+                        elif sys.argv[i] == '--Sort':
+                            Sort = True
+                        else:
+                            show_usage()
+                            exit(1)
+                    else:
+                        for c in range(1,len(sys.argv[i])):
+                            if sys.argv[i][c] == 'h':
+                                Usage = True
+                            elif sys.argv[i][c] == 'r':
+                                Recursive = True
+                            elif sys.argv[i][c] == 'i':
+                                Ignorecase = True
+                            elif sys.argv[i][c] == 'l':
+                                Searchlink = True                                              
+                            elif sys.argv[i][c] == 'd':
+                                Searchdir = True
+                            elif sys.argv[i][c] == 'n':
+                                Onlyname = True
+                            elif sys.argv[i][c] == 'f':
+                                Searchfile = True
+                            elif sys.argv[i][c] == 'm':
+                                Mark = True 
+                            elif sys.argv[i][c] == 's':
+                                Sort = True
+                            else:
+                                show_usage()
+                                exit(1)
+                elif i == ( len(sys.argv) - 2 ):
+                    Main_Dir = sys.argv[i]
+                elif i == ( len(sys.argv) - 1 ):
+                    Text = sys.argv[i]
+                    if Ignorecase:
+                        Pattern = Text.lower()
+                    else:
+                        Pattern = Text
+                else:
+                    show_usage()
+                    exit(1)
+    if Usage:
+        show_usage()
+        exit(0)
+    if Searchdir is False and Searchlink is False and Searchfile is False:
+        Searchdir,Searchfile,Searchlink = True,True,True
+    if Main_Dir == '':
+        Main_Dir ='.'
+    if Pattern == '':
+        Pattern = '.*'
+    pys = Py_Search(Main_Dir,Pattern,Recursive,Ignorecase,Searchdir,Searchfile,Searchlink,Onlyname,Mark,Sort,Silent=False)
     pys.main()
