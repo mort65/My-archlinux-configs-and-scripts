d1f5ee0275222619ee2ee2f785af19ae7edec85a
mort65 <mohsenbani65@gmail.com>
2018-02-20 12:20:37 +0000

    Update obwc.py

--- bin/openbox/obwc.py
+++ bin/openbox/obwc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-#changing wallpaper of openbox
+
+#This script changes desktop wallpaper with feh
 
 import os
 import glob
@@ -8,23 +9,28 @@ import random
 
 time.sleep(15)
 homedir=os.path.expanduser('~')
-logfilename =''.join([homedir,'/bin/openbox/tmp/.lastimage.txt'])
+logfilename =''.join([homedir,'/bin/openbox/tmp/.prev_wallpapers.log'])
 images = glob.glob(''.join([homedir,'/Pictures/Desktop/*.jpg']))
 if len(images)== 0:
     exit(1);
 if os.path.isfile(logfilename):
     with open(logfilename , 'r+') as logfile:
-        lastimagename = logfile.read().replace('\n', '')
-        if lastimagename in images:
-            images.remove(lastimagename)
+        previousimages = logfile.read().split('\n')
+        if len(previousimages) < min(len(images),1000): 
+            for image in previousimages:
+                if image in images:
+                    images.remove(image)
+            if len(previousimages) > 0:
+                logfile.write('\n')
+        else:
+             logfile.seek(0)
         image=images[random.randint(0,len(images)-1)]
-        logfile.seek(0)
         logfile.write(image)
         logfile.truncate()
 else:
    with open(logfilename,"w") as logfile:
         image=images[random.randint(0,len(images)-1)]
         logfile.write(image)
-command='/usr/bin/feh --bg-fill \'{}\''.format(image)
+command='/usr/bin/feh -q --bg-fill \'{}\''.format(image)
 os.system(command)
 logfile.close()
