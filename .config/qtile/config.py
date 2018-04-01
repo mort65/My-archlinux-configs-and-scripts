9eab44697ca210f9e4b6b436d3d4259853132bc4
mort65 <mohsenbani65@gmail.com>
2018-04-01 22:17:49 +0430

    Update config.py

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -435,7 +435,7 @@ screens = [
                                 highlight_color=['1A2024','060A0F']
                                ),
                 widget.Prompt(fontsize=13,cursor_color='FFFFFF',foreground='FAF0A8',background='271B1B'),
-                widget.WindowName(foreground='8ABDAF'),
+                widget.WindowName(foreground='7ABCA0'),
                 widget.Net(interface='enp3s0',foreground='FFAAFF'),
                 widget.GenPollText(func=get_ctemp, update_interval=5, foreground="88D2FF"),
                 widget.GenPollText(func=get_gtemp, update_interval=5, foreground="88D2FF"),
@@ -450,7 +450,7 @@ screens = [
             ],
             22,
             background=['1A2024','060A0F'],
-            opacity=0.95,
+            opacity=0.96,
         ),
     ),
 ]
