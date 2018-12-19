df9c84941c613e34292dbc83297c62887bbe3fbc
Mort65 <Mort65@protonmail.com>
2018-12-20 03:03:25 +0330

    deleted:    .bashrc 	deleted:    .config/compton.conf 	deleted:    .config/mpv/input.conf 	deleted:    .config/mpv/mpv.conf 	deleted:    .conky/Gotham3/Gotham3 	deleted:    .conky/conky-startup.sh 	deleted:    .conky/conky-startup1.sh 	deleted:    .dropbox-dist/dropboxd 	deleted:    .script/.update-arch-in-tmux.sh 	deleted:    .script/cpu.py 	deleted:    .script/i3-jdate 	deleted:    .script/openbox-jdate 	deleted:    .script/per_cpu.py 	deleted:    .script/uptime.py 	deleted:    .script/xfcepanel-jdate 	deleted:    .script/xfcepanel-tfm 	deleted:    .script/xfcepanel-totalupdatesavail 	deleted:    .zfuncs/syncit 	deleted:    .zshenv 	deleted:    .zshrc 	deleted:    bin/cpuhogs 	deleted:    bin/ctemp 	deleted:    bin/enablenotifyforcronie 	deleted:    bin/gtemp 	deleted:    bin/isomount 	deleted:    bin/logoff 	deleted:    bin/logupdates 	deleted:    bin/memhogs 	deleted:    bin/miso 	deleted:    bin/orphs 	deleted:    bin/pacs 	deleted:    bin/pysearch 	deleted:    bin/randcowsay 	deleted:    bin/randponysay 	deleted:    bin/randsay.py 	deleted:    bin/refresh-net 	deleted:    bin/rm-orphs 	deleted:    bin/rm-pacmanlock 	deleted:    bin/rm-pacnew 	deleted:    bin/rsearch 	deleted:    bin/search 	deleted:    bin/startmux 	deleted:    bin/totalavailupdates 	deleted:    bin/totalfiles 	deleted:    bin/totalfreemem 	deleted:    bin/totalfreeswap 	deleted:    bin/totalusedmem 	deleted:    bin/totalusedswap 	deleted:    bin/uiso 	deleted:    bin/update-arch 	deleted:    bin/wallpaperswitcher.py 	deleted:    oblogout.conf

--- .conky/conky-startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-USERNAME=${SUDO_USER:-$(id -u -n)}
-HOMEDIR="/home/$USERNAME"
-sleep 20s
-killall conky
-cd "HOMEDIR/.conky/Gotham3"
-conky -c "HOMEDIR/.conky/Gotham3/Gotham3" &
--- .conky/conky-startup1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-USERNAME=${SUDO_USER:-$(id -u -n)}
-HOMEDIR="/home/$USERNAME"
-if [[ ( $DESKTOP_SESSION == "/usr/share/xsessions/xfce" ) || ( $DESKTOP_SESSION == "/usr/share/xsessions/mate" ) || ( $DESKTOP_SESSION == "/usr/share/xsessions/budgie-desktop" ) || ( $DESKTOP_SESSION == "/usr/share/xsessions/cinnamon" ) || ( $DESKTOP_SESSION == "/usr/share/xsessions/lxqt" ) || ( $DESKTOP_SESSION == "/usr/share/xsessions/openbox" ) ]]
-then
-   . "$HOMEDIR/.conky/conky-startup.sh"
-fi
-exit
