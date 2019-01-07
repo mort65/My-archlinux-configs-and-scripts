5c66099f43a4a29572207328c587b015ac9a6626
mort65 <Mort65@protonmail.com>
2019-01-07 15:57:40 +0330

    modified:   .script/.update-arch-in-tmux.sh 	modified:   bin/ctemp 	new file:   bin/entr-web 	modified:   bin/gtemp 	new file:   bin/reload-browser 	new file:   bin/txt2qr 	modified:   bin/update-arch

--- .script/.update-arch-in-tmux.sh
+++ .script/.update-arch-in-tmux.sh
@@ -48,12 +48,12 @@ function help {
     echo "A script for updating archlinux."
     echo ""
     echo "  -a, --aur       Refresh and synchronize all normal and aur package databases"
-    echo "  -o  --poweroff  Shutdown computer if the script finished without error"         
+    echo "  -o  --poweroff  Shutdown computer if the script finished without error"
     echo "  -e  --reboot    Restart computer if the script finished without error"
     echo "  -f  --freeze    Hibernate computer if the script finished without error"
     echo "  -u  --suspend   Suspend computer if the script finished without error"
     echo "  -g  --logout    Logout if the script finished without error"
-    echo "  --shutdown      Shutdown computer if the script finished without error"     
+    echo "  --shutdown      Shutdown computer if the script finished without error"
     echo "  --restart       Restart computer if the script finished without error"
     echo "  --hibernate     Hibernate computer if the script finished without error"
     echo "  --sleep         Suspend computer if the script finished without error"
@@ -61,8 +61,8 @@ function help {
     echo "  -s, --sync      Refresh and synchronize normal package databases"
     echo "  -y, --refresh   Force the refresh of package databases"
     echo "  -n, --nonamcap  Disable checking packages with namcap"
-    echo "  -r  --norss     Disable checking for archlinux news" 
-    echo "  -p, --nopac     Disable checking for pacnew files" 
+    echo "  -r  --norss     Disable checking for archlinux news"
+    echo "  -p, --nopac     Disable checking for pacnew files"
     echo "  -i, --install   Install a package"
     echo "  -b, --build     Build a package"
     echo "  -l  --nolog     Disable logging"
@@ -105,7 +105,7 @@ if [ -n "$1" ]; then #non-empty
         echo -e $red"Error:$reset Invalid arguments '${Args}'"
         echo ""
 		usage
-	  fi 
+	  fi
     elif [[ $PARAM =~ ^--[Mm][Ii][Rr][Rr][Oo][Rr]$ ]] || [[ $PARAM =~ ^-[Mm]$ ]]; then
 	  if [[ $Mirror == "" ]]; then
         Mirror="Yes"
@@ -185,7 +185,7 @@ if [ -n "$1" ]; then #non-empty
         echo -e $red"Error:$reset Invalid arguments '${Args}'"
         echo ""
 		usage
-	  fi  
+	  fi
     elif [[ $PARAM =~ ^--[Ss][Uu][Ss][Pp][Ee][Nn][Dd]$ ]] || [[ $PARAM =~ ^--[Ss][Ll][Ee][Ee][Pp]$ ]] || [[ $PARAM =~ ^-[Uu]$ ]]; then
       if [[ $Sleep == "" ]] && [[ $Shutdown == "" ]] && [[ $Reboot == "" ]] && [[ $Hibernate == "" ]] && [[ $Logout == "" ]]; then
         Sleep="Yes"
@@ -201,7 +201,7 @@ if [ -n "$1" ]; then #non-empty
         echo -e $red"Error:$reset Invalid arguments '${Args}'"
         echo ""
 		usage
-	  fi 
+	  fi
     elif [[ $PARAM =~ ^--[Ll][Oo][Gg][Oo][Ff][Ff]$ ]] || [[ $PARAM =~ ^--[Ll][Oo][Gg][Oo][Uu][Tt]$ ]] || [[ $PARAM =~ ^-[Gg]$ ]]; then
       if [[ $Sleep == "" ]] && [[ $Shutdown == "" ]] && [[ $Reboot == "" ]] && [[ $Hibernate == "" ]] && [[ $Logout == "" ]]; then
         Logout="Yes"
@@ -246,7 +246,7 @@ if [ -n "$1" ]; then #non-empty
 				echo -e $red"Error:$reset Invalid arguments '${Args}'"
 				echo ""
 				usage
-			fi		
+			fi
         elif [[ $char =~ [Yy] ]]; then
 			if [[ $Refresh == "" ]]; then
 				Refresh="Yes"
@@ -310,7 +310,7 @@ if [ -n "$1" ]; then #non-empty
                 echo -e $red"Error:$reset Invalid arguments '${Args}'"
                 echo ""
                 usage
-            fi  
+            fi
         elif [[ $char =~ [Oo] ]]; then
             if [[ $Sleep == "" ]] && [[ $Shutdown == "" ]] && [[ $Reboot == "" ]] && [[ $Hibernate == "" ]] && [[ $Logout == "" ]]; then
                 Shutdown="Yes"
@@ -318,7 +318,7 @@ if [ -n "$1" ]; then #non-empty
                 echo -e $red"Error:$reset Invalid arguments '${Args}'"
                 echo ""
                 usage
-            fi 
+            fi
         elif [[ $char =~ [Ee] ]]; then
             if [[ $Sleep == "" ]] && [[ $Shutdown == "" ]] && [[ $Reboot == "" ]] && [[ $Hibernate == "" ]] && [[ $Logout == "" ]]; then
                 Reboot="Yes"
@@ -334,7 +334,7 @@ if [ -n "$1" ]; then #non-empty
                 echo -e $red"Error:$reset Invalid arguments '${Args}'"
                 echo ""
                 usage
-            fi 
+            fi
         elif [[ $char =~ [Ff] ]]; then
             if [[ $Sleep == "" ]] && [[ $Shutdown == "" ]] && [[ $Reboot == "" ]] && [[ $Hibernate == "" ]] && [[ $Logout == "" ]]; then
                 Hibernate="Yes"
@@ -387,7 +387,7 @@ if [[ ! $PacOff == "Yes" ]]; then
         read -p "Press enter to exit..."
         exit 1
     else
-        echo 
+        echo
         echo "No pacnew file found"
     fi
     echo
@@ -413,7 +413,7 @@ if [[ ! $RSSOff == "Yes" ]]; then
             echo -e $red" !!!$reset New arch linux news entry found $red!!!$reset"
             echo
             [ -f $TMPFILE ] && cat "${TMPFILE}" 2> /dev/null
-            sleep 3		
+            sleep 3
             /usr/bin/mv -f $TMPFILE $RSSFILE
             { /usr/bin/rsstail -n 5 -1 -l -d -H -u https://www.archlinux.org/feeds/news/ | /usr/bin/less ; } || { echo -e $red"Error:$reset Cannot download the rss content" ; read -p "Press enter to exit..." ; exit 1; }
             exit 0
@@ -464,7 +464,7 @@ if [[ $Install == "Yes" ]] || [[ $Build == "Yes" ]]; then
         echo ""
         { /usr/bin/find . -maxdepth 1 -name '*.pkg.tar.xz' -exec $SUDO /usr/bin/pacman -U {} \; ; } || { echo "" ; echo -e $red"Error:$reset Cannot install the package" ; echo "" ; read -p "Press enter to exit..." ; exit 1; }
     fi
-fi	
+fi
 if [[ $Sync == "Yes" ]]; then
     echo
     echo -e $blue"===>$reset Checking for updates..."
@@ -476,7 +476,7 @@ if [[ $Sync == "Yes" ]]; then
             else
                 echo -e $red"Error:$reset Yaourt not found"
                 exit 1
-            fi 
+            fi
         elif [ -x /usr/bin/pacmatic ]; then
             { $SUDO /usr/bin/pacmatic -Syu ; } || { echo -e $red"Error:$reset Cannot update the system" ; exit 1; }
         else
@@ -499,7 +499,7 @@ if [[ $Sync == "Yes" ]]; then
 fi
 echo
 if [[ ! $RSSOff == "Yes" ]]; then
-    [ -f $RSSFILE ] && cat "${RSSFILE}" | 2> /dev/null
+    [ -f $RSSFILE ] && cat "${RSSFILE}" 2> /dev/null
 fi
 echo
 if [[ ! $LoggingOff == "Yes" ]]; then
