de570594044520c65336e834c6cce1ec7581468e
mort65 <Mort65@protonmail.com>
2019-02-03 01:59:00 +0330

    modified:   dwm-patched/PKGBUILD 	modified:   dwm-patched/dwm 	modified:   dwm-patched/dwm-patched-6.1-3-x86_64.pkg.tar.xz 	modified:   dwm-patched/dwm.c 	modified:   dwm-patched/dwm.o

--- dwm-patched/dwm.c
+++ dwm-patched/dwm.c
@@ -1798,7 +1798,7 @@ resizeclient(Client *c, int x, int y, int w, int h)
 		gapincr = gapoffset = 0;
 	} else {
 		/* Remove border and gap if layout is monocle or only one client */
-		if (selmon->lt[selmon->sellt]->arrange == monocle || (n == 1 && !c->issticky && !c->ispermanent)) {
+		if (!c->issticky && !c->ispermanent && (selmon->lt[selmon->sellt]->arrange == monocle || n == 1 )) {
 			gapoffset = 0;
 			gapincr = -2 * borderpx;
 			wc.border_width = 0;
