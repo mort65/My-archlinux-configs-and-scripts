a9e6c2545d4781a66591dd53e3af48cade48d7d5
mort65 <Mort65@protonmail.com>
2019-02-05 12:53:24 +0330

    modified:   dwm-patched/PKGBUILD 	modified:   dwm-patched/config.h 	modified:   dwm-patched/dwm 	modified:   dwm-patched/dwm-patched-6.1-3-x86_64.pkg.tar.xz 	modified:   dwm-patched/dwm.o

--- dwm-patched/config.h
+++ dwm-patched/config.h
@@ -25,7 +25,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char buttonbar[]       = "<0>";
+static const char buttonbar[]       = "::";
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 
@@ -168,9 +168,9 @@ void ruleshook(Client *c)
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_blue, "-sf", col_white, NULL };
 static const char *roficmd[] = { "rofi", "-modi", "combi#window#run#drun", "-show", "combi", "-combi-modi", "window#run#drun", NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "mlterm", NULL, NULL, NULL, NULL, "1", NULL };
 static const char *termcmd1[]  = { "urxvt", NULL, NULL, NULL, NULL, "1", NULL };
-static const char *termcmd2[]  = { "mlterm", NULL };
+static const char *termcmd2[]  = { "st", NULL, NULL, NULL, NULL, "1", NULL };
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 static const char *firefox[] = { "firefox", NULL, NULL, NULL, "Firefox", "2", NULL};
@@ -199,9 +199,9 @@ static Key keys[] = {
 /*	 type            modifier                     key        function        argument */
 	{KeyPress,	 MODKEY,                       XK_o,      spawn,          {.v = dmenucmd } },
 	{ KeyPress,	 MODKEY,                       XK_p,      spawn,          {.v = roficmd } },
-	{ KeyPress,	 MODKEY|ControlMask,           XK_Return, spawn,          {.v = termcmd } },
+	{ KeyPress,	 MODKEY|ControlMask,           XK_Return, runorraise,          {.v = termcmd } },
 	{ KeyPress,	 MODKEY|ShiftMask,             XK_Return, runorraise,          {.v = termcmd1 } },
-	{ KeyPress,	 MODKEY|ALTMODKEY,             XK_Return, spawn,          {.v = termcmd2 } },
+	{ KeyPress,	 MODKEY|ALTMODKEY,             XK_Return, runorraise,          {.v = termcmd2 } },
         { KeyPress,	 MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
 	{ KeyPress,	 MODKEY,                       XK_b,      togglebar,      {0} },
 	{ KeyPress,	 MODKEY|ShiftMask,             XK_b,      spawn,          SHCMD("~/.script/dwm-toggle_dzen") },
@@ -284,7 +284,7 @@ static Key keys[] = {
         { KeyPress,	 MODKEY|ControlMask,           XK_e,      runorraise,     {.v = texteditor} },
         { KeyPress,	 MODKEY|ControlMask,           XK_Delete, runorraise,     {.v = lxtask} },
         { KeyPress,	 MODKEY,                       XK_Delete, spawn,          {.v = htop} },
-        { KeyRelease,	 MODKEY,                       XK_F1,     spawn,          SHCMD("jgmenu_run") },
+        { KeyPress,	 MODKEY,                    XK_F1,   spawn,          SHCMD("jgmenu_run") },
         { KeyRelease,	 MODKEY,                       XK_Print,  spawn,          SHCMD("~/bin/winshot") },
         { KeyRelease,	 0,                            XK_Print,  spawn,          SHCMD("~/bin/screenshot") },
 
@@ -327,7 +327,7 @@ static Signal signals[] = {
 	{ 108,          view,       {.ui = 1 << 7} },
 	{ 109,          view,       {.ui = 1 << 8} }, */
 	{ 110,          runorraise,       {.v = filemanager} },
-	{ 111,          runorraise,       {.v = termcmd1} },
+	{ 111,          runorraise,       {.v = termcmd} },
 	{ 112,          runorraise,       {.v = firefox} },
 
 };
