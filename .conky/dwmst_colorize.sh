34a4449754d31b2c7919fd703131493e7f0baa54
mort65 <Mort65@protonmail.com>
2019-02-22 12:39:23 +0330

    modified:   .conky/dwmst_colorize.sh

--- .conky/dwmst_colorize.sh
+++ .conky/dwmst_colorize.sh
@@ -5,11 +5,11 @@ WARM=80
 NUM="$(echo "$1" | tr -dc '0-9.\n')"
 
 if (( $(bc -l<<<"$NUM<$COOL") )); then
-	echo -e "^fg()$1"    # COOL
+	echo -e "^fg()$1"        # COOL
 elif (( $(bc -l<<<"$NUM>=$WARM") )); then
-	echo -e "^fg(#ff0066)$1"  # HOT
+	echo -e "^fg(#ff0066)$1" # HOT
 else
-	echo -e "^fg(#ffff00)$1"                        # WARM
+	echo -e "^fg(#ffff00)$1" # WARM
 fi
 
 exit 0
